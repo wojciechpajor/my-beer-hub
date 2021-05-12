5fced273eca4d30b55bed37dea3bb3c9028be2e6
wojciechpajor <pwojtekp3@gmail.com>
2021-05-12 15:08:03 +0200

    Added upload file to AddBeer funcjonality

--- src/Components/AddBeer/AddBeer.js
+++ src/Components/AddBeer/AddBeer.js
@@ -20,43 +20,59 @@ const AddBeer = () => {
 
     }
 
-    const submitBeer = () => {
-        if((rating>0) && (rating<=5)){
-        addBeer({name,mark,type,alk,origin,rating,url})
+
+    const submitBeer = (e) => {
+        e.preventDefault()
+        if ((rating > 0) && (rating <= 5)) {
+            addBeer({ name, mark, type, alk, origin, rating, url })
+        
         }
-        else { 
+        else {
             return (
                 window.alert("Error: Wrong data")
             )
         }
     }
 
-    return(
-        <div className = "bodyStyle"> 
-                    <div className = "wrapperStyle">
-            <div className = "formStyle">
-                <div class ="inputStyle">
-                    <input required type="text" class = "input" placeholder="Beer Name" onChange ={(e) => setName(e.target.value)}/>
-                    <input type="text" class = "input" placeholder="Beer Mark" onChange ={(e) => setMark(e.target.value)} />
-                    <input type="text" class = "input" placeholder="Beer Type" onChange ={(e) => setType(e.target.value)} />
-                    <input type="number" class = "input" max = {100} min = {0} placeholder="Beer Alk" onChange ={(e) => setAlk(e.target.value)} />
-                    <input type="text" class = "input" placeholder="Beer Origin" onChange ={(e) => setOrigin(e.target.value)} />
-                    <input type="number" class = "input" min = {0} max = {5} placeholder="Beer Rating" onChange ={(e) => setRating(e.target.value)} />
-                    <input type="text" class = "input" placeholder="firebase image url" onChange ={(e) => setUrl(e.target.value)} />    
-                    <div className = "buttonStyle" onClick={submitBeer} >Send</div>
-                    
+    const onFileChange = async (e) => {
+        const file = e.target.files[0]
+        const storageRef = firebase.storage().ref()
+        const fileRef = storageRef.child(file.name)
+        await fileRef.put(file)
+        const fileUrl = await fileRef.getDownloadURL()
+        await setUrl(fileUrl)
+
+    }
+
+    return (
+        <div className="bodyStyle">
+            <div className="wrapperStyle">
+                <div className="formStyle">
+                    <div class="inputStyle">
+                        <input required type="text" class="input" placeholder="Beer name" onChange={(e) => setName(e.target.value)} ></input>
+                        <input type="text" class="input" placeholder="Beer Mark" onChange={(e) => setMark(e.target.value)} />
+                        <input type="text" class="input" placeholder="Beer Type" onChange={(e) => setType(e.target.value)} />
+                        <input type="number" class="input" max={100} min={0} placeholder="Beer Alk" onChange={(e) => setAlk(e.target.value)} />
+                        <input type="text" class="input" placeholder="Beer Origin" onChange={(e) => setOrigin(e.target.value)} />
+                        <input type="number" class="input" min={0} max={5} placeholder="Beer Rating" onChange={(e) => setRating(e.target.value)} />
+                        <div>
+                        <p class = "labelText">Upload beer photo:</p>
+                        <input type="file" class="input" onChange={onFileChange} accept =".png, .jpg" />
+                        </div>
+                        <div className="buttonStyle" onClick={submitBeer} >Send</div>
+
+
+                    </div>
 
                 </div>
 
             </div>
-
-        </div>
         </div>
 
     )
 
-    
-    
+
+
 }
 
 export default AddBeer;
--- src/firebase.js
+++ src/firebase.js
@@ -3,6 +3,9 @@ import "firebase/firestore";
 import 'firebase/storage';
 
 const firebaseConfig = {
+  appId: process.env.REACT_APP_API_APP_ID,
+  databaseURL: process.env.REACT_APP_API_DATABSE_URL,
+  locationID: process.env.REACT_APP_API_LOCATION_ID,
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
