5aee13623503e9e14b5d1992dc8358d8ab992b75
wojciechpajor <pwojtekp3@gmail.com>
2021-05-13 12:17:17 +0200

    Gallerybeer code cleanup using destructor

--- src/Components/Gallery/GalleryBeer.js
+++ src/Components/Gallery/GalleryBeer.js
@@ -1,17 +1,17 @@
 import "./GalleryBeer.css"
 
-function GalleryBeer(beer)  {
+function GalleryBeer(name, mark, type, alk, origin, rating, url)  {
     const altImage = "https://firebasestorage.googleapis.com/v0/b/my-beer-hub-355e2.appspot.com/o/No-photo.png?alt=media&token=11ec7801-2545-4b6d-85a4-896a27d957de"
     return (
         <div className = "BeerViewStyle">
-            <img className = "beerImageStyle" src = {beer.props.url} alt={altImage}></img>
+            <img className = "beerImageStyle" src = {url} alt={altImage}></img>
             <div>
-            <li className = "BeerNameStyle">Name: {beer.props.name}</li>
-            <li>Mark: {beer.props.mark}</li>
-            <li>Type: {beer.props.type}</li>
-            <li>Alk: {beer.props.alk}</li>
-            <li>Origin: {beer.props.origin}</li>
-            <li>rating: {beer.props.rating}/5</li>
+            <li className = "BeerNameStyle">Name: {name}</li>
+            <li>Mark: {mark}</li>
+            <li>Type: {type}</li>
+            <li>Alk: {alk}</li>
+            <li>Origin: {origin}</li>
+            <li>rating: {rating}/5</li>
             </div>
             
         </div>
