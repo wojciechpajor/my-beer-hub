a33a4a0e76e2b3963895fca444ea6e2df364e609
Wojciech Pajor <wpajor@student.agh.edu.pl>
2022-09-29 03:03:11 +0200

    add comment section

--- src/Components/DetailedBeerView/DetailedBeerView.tsx
+++ src/Components/DetailedBeerView/DetailedBeerView.tsx
@@ -1,9 +1,11 @@
-import React, {FC, useEffect, useState} from "react"
-import {Beer} from "../../Models/Beer.interface"
+import React, {ChangeEvent, FC, useEffect, useState} from "react"
+import {Beer, UserComment} from "../../Models/Beer.interface"
 import {useParams} from 'react-router-dom';
 import firebase from "../../firebase";
 import "./DetailedBeerView.css"
-import {Divider, Avatar, Grid, Paper} from "@mui/material";
+import {Divider, Avatar, Grid, Paper, Button, TextField} from "@mui/material";
+import firestore = firebase.firestore;
+import Timestamp = firebase.firestore.Timestamp;
 
 type SelectedBeerShowcaseProps = {
     beer: Beer
@@ -11,30 +13,56 @@ type SelectedBeerShowcaseProps = {
 
 export const DetailedBeerView: FC = () => {
     const [beers, setBeers] = useState<Beer[]>([]);
+    const [comments, setComments] = useState<UserComment[]>([])
     const [selectedBeer, setSelectedBeer] = useState<Beer>()
+    const [addingCommentFieldValue, setAddingCommentFieldValue] = useState<string>()
 
     const myID = useParams().beerID
+    const beersRef = firebase.firestore().collection('Beers')
+    const commentsRef = firebase.firestore().collection(`Beers/${myID}/Comments`)
 
     const getBeers = () => {
-        firebase.firestore()
-            .collection('Beers')
-            .onSnapshot(snapshot => {
-                const mappedBeers: Beer[] = snapshot.docs.map(doc => ({
-                    ...doc.data() as Beer,
-                    id: doc.id,
-                }));
-                setBeers(mappedBeers)
-            })
+        beersRef.onSnapshot(snapshot => {
+            const mappedBeers: Beer[] = snapshot.docs.map(doc => ({
+                ...doc.data() as Beer,
+                id: doc.id,
+            }));
+            setBeers(mappedBeers)
+
+        })
+
     }
+
+    const getComments = () => {
+        commentsRef.onSnapshot(snapshot => {
+            const mappedComments: UserComment[] = snapshot.docs.map(doc => ({
+                ...doc.data() as UserComment
+            }));
+            setComments(mappedComments)
+        })
+    }
+
+    const addComment = () => {
+        commentsRef.doc()
+            .set({author:"testAuthor",content: "test content",date: firebase.firestore.FieldValue.serverTimestamp()})
+            .then(() => console.log("success"))
+    }
+
 
     useEffect(() => {
         getBeers()
+        getComments()
     }, [])
+
     useEffect(() => {
         setSelectedBeer(beers.find(beer => beer.id === myID.toString()))
     }, [beers])
 
-    //console.log(myID, beers, selectedBeer)
+    const handleAddingCommentTextFieldChange = (e: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+        e.preventDefault()
+        setAddingCommentFieldValue(e.target.value)
+    }
+
 
     const SelectedBeerShowcase: FC<SelectedBeerShowcaseProps> = ({beer}) => {
         const altImage = "https://firebasestorage.googleapis.com/v0/b/my-beer-hub-355e2.appspot.com/o/No-photo.png?alt=media&token=11ec7801-2545-4b6d-85a4-896a27d957de"
@@ -63,31 +91,48 @@ export const DetailedBeerView: FC = () => {
                         </div>
                     </div>
                     <div className="commentsContainer">
-                        {beer.comments
+                        {comments.length
                             ? <Paper style={{padding: "40px 20px", backgroundColor: "#fff75a"}}>
-                            <>
-                                {beer.comments.map(comment => <Grid container wrap="nowrap" spacing={2}>
-                                        <Grid item>
-                                            <Avatar alt="Grzegorz Karaś" src={"imgLink"}/>
+                                <>
+                                    {comments.map(comment => <Grid container wrap="nowrap" spacing={2}>
+                                            <Grid item>
+                                                <Avatar alt="Grzegorz Karaś" src={"imgLink"}/>
+                                            </Grid>
+                                            <Grid justifyContent="left" item xs zeroMinWidth>
+                                                <h4 style={{margin: 0, textAlign: "left"}}>{comment.author}</h4>
+                                                <p style={{textAlign: "left"}}>{comment.content}</p>
+                                                <p style={{
+                                                    textAlign: "left",
+                                                    color: "gray"
+                                                }}>{comment.date.toDate().getDay() + " days ago"}</p>
+                                            </Grid>
                                         </Grid>
-                                        <Grid justifyContent="left" item xs zeroMinWidth>
-                                            <h4 style={{margin: 0, textAlign: "left"}}>{comment.author}</h4>
-                                            <p style={{textAlign: "left"}}>{comment.content}</p>
-                                            <p style={{textAlign: "left", color: "gray"}}>{comment.date.toDate().toString()}</p>
-                                        </Grid>
-                                    </Grid>
-                                )}
-                            </>
-                        </Paper>
-                            : <Paper style={{padding: "40px 20px", backgroundColor: "#fff75a",width:"80vw"}}>
+                                    )}
+                                </>
+                            </Paper>
+                            : <Paper style={{padding: "40px 20px", backgroundColor: "#fff75a", width: "80vw"}}>
                                 <Grid container wrap="nowrap" spacing={2}>
                                     <Grid justifyContent="left" item xs zeroMinWidth>
-                                        <p style={{textAlign: "center"}}>Ten artykół nie posiada jeszcze komentarza</p>
+                                        <p style={{textAlign: "center"}}>Ten artykuł nie posiada jeszcze komentarza</p>
                                     </Grid>
                                 </Grid>
                             </Paper>
                         }
                     </div>
+                    <div style={{width:"80%", padding:20,display:"flex", alignItems: "center",justifyContent:"center"}}>
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Multiline"
+                                multiline
+                                rows={4}
+                                placeholder={"Podaj treść komentarza"}
+                                value={addingCommentFieldValue}
+                                onChange={(event) => setAddingCommentFieldValue(event.target.value)}
+                                style={{width:"100%"}}
+                            />
+                        <Button style={{margin:5}} variant={"contained"} onClick={() => addComment()}>Dodaj Komentarz</Button>
+                    </div>
+
                 </div>
             </div>
         )
